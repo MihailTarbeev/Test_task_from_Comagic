ba08dc4c4c72e7ab62132632e7bc3d9fac0361e5
Михаил <tarb3@yandex.ru>
2023-11-01 11:36:59 +0300

    Fifth commit

--- node/service.py
+++ node/service.py
@@ -5,7 +5,7 @@ from uc_flow_nodes.schemas import NodeRunContext
 from uc_flow_nodes.service import NodeService
 from uc_flow_nodes.views import info, execute
 from uc_flow_schemas import flow
-from uc_flow_schemas.flow import Property, CredentialProtocol, RunState, DisplayOptions
+from uc_flow_schemas.flow import Property, CredentialProtocol, RunState, DisplayOptions, OptionValue
 from uc_http_requester.requester import Request
 
 
@@ -35,11 +35,107 @@ class NodeType(flow.NodeType):
         ),
         Property(
             displayName='Вернуть тип Число/Текст',
-            name='Переключатель',
+            name='Переключатель типов',
             type=Property.Type.BOOLEAN,
             description='Выбор типа возвращаемых данных',
             required=True,
             default=False,
+        ),
+        Property(
+            displayName='Включить поля',
+            name='Переключатель полей',
+            type=Property.Type.BOOLEAN,
+            description='Отображает два поля с выпадающими списками',
+            required=True,
+            default=False,
+        ),
+        Property(
+            displayName='Список 1',
+            name='Cписок 1',
+            type=Property.Type.OPTIONS,
+            noDataExpression=True,
+            displayOptions=DisplayOptions(
+                show={
+                    'Переключатель полей': [
+                        True,
+                    ],
+                },
+            ),
+            options=[
+                OptionValue(
+                    name='Значение 1',
+                    value='Значение 1',
+                ),
+                OptionValue(
+                    name='Значение 2',
+                    value='Значение 2',
+                ),
+            ],
+        ),
+        Property(
+            displayName='Список 2',
+            name='Cписок 2',
+            type=Property.Type.OPTIONS,
+            noDataExpression=True,
+            displayOptions=DisplayOptions(
+                show={
+                    'Переключатель полей': [
+                        True,
+                    ],
+                },
+            ),
+            options=[
+                OptionValue(
+                    name='Значение 1',
+                    value='Значение 1',
+                ),
+                OptionValue(
+                    name='Значение 2',
+                    value='Значение 2',
+                ),
+            ],
+        ),
+        Property(
+            displayName='Почта',
+            name='Почта',
+            placeholder = 'Поле для ввода почты',
+            type=Property.Type.EMAIL,
+            displayOptions=DisplayOptions(
+                show={
+                    'Переключатель полей': [
+                        True,
+                    ],
+                    'Cписок 1':
+                    [
+                        'Значение 1'
+                    ],
+                    'Cписок 2':
+                    [
+                        'Значение 1'
+                    ],
+                },
+            ),
+        ),
+        Property(
+            displayName='Дата и время',
+            name='Дата и время',
+            placeholder = 'Поле для ввода даты и времени',
+            type=Property.Type.DATETIME,
+            displayOptions=DisplayOptions(
+                show={
+                    'Переключатель полей': [
+                        True,
+                    ],
+                    'Cписок 1':
+                    [
+                        'Значение 2'
+                    ],
+                    'Cписок 2':
+                    [
+                        'Значение 2'
+                    ],
+                },
+            ),
         ),
     ]
 
@@ -53,7 +149,7 @@ class ExecuteView(execute.Execute):
     async def post(self, json: NodeRunContext) -> NodeRunContext:
         try:
             result = int(json.node.data.properties['Текст']) + json.node.data.properties['Число']
-            is_text = json.node.data.properties['Переключатель']
+            is_text = json.node.data.properties['Переключатель типов']
             if is_text:
                 result = str(result)
             await json.save_result({
